a5cdc4b9c7d4c747ba0eff2de14e097a916c82d5
unknown <rozczko@hotmail.com.com>
2023-09-17 22:13:56 +0200

    fixed image

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost:4000', 'piouraterie.vercel.app']
-    // remotePatterns: [
-    //   {
-    //     protocol: '**',
-    //     hostname: '**',
-    //     port: '*',
-    //     pathname: '/public/**',
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'piouraterie-backend.vercel.app',
+        // port: '4000' | '',
+        pathname: '/public/**',
+      },
+    ],
   },
 }
 
